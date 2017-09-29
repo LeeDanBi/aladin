ad558b4b8577f9a034c4bdeeb73f69131acccdee
leedanbi <goldq0531@gmail.com>
2017-09-29 09:34:14 +0900

    중고 페이지 제외 완성

--- js/src/main.js
+++ js/src/main.js
@@ -1,76 +1,5 @@
 (function($){
 
-  // gnb menu ___________________________
-
-  var windowS = $(window);
-  var addBanner= $('#addBanner');
-  var gnb = $('#gnb');
-  var menuButton = $('#menuButton');
-  var buttonDeco = $('#buttonDeco')
-
-  // menuButton.slideUp();
-
-  windowS.on('scroll',function(){
-  var wst = $(this).scrollTop();
-  var mot = addBanner.offset().top;
-
-
-  // if(wst>=mot){
-  //   menuButton.slideDown().css({position:'fixed', top: '0'}).removeClass('button_deco');
-  //   if(gnb.css('position')=='fixed' && gnb.css('top')=='0'){
-  //   menuButton.css({position:'absolute', top: '60px'});
-  //   }
-  // }else{
-  //   
-  //   menuButton.slideUp();
-  //   menuButton.css({position:'absolute', top: '60px'});
-  //   if(gnb.css('position')=='fixed'){
-  //     gnb.css({position:'relative', top: 0});
-  //   }
-  // }
-
- if(wst>=mot){
-  gnb.addClass('fixed');
-  menuButton.slideDown();
-  if(!buttonDeco.hasClass('button_deco')){
-  gnb.css({transform:'translateY(-60px)'});
-  }
- }else{
-  menuButton.slideUp();
-  gnb.removeClass('fixed');
-  buttonDeco.removeClass('button_deco');
-  gnb.css({transform:'translateY(0)'});
- }
-
-
-  });
-
-  menuButton.on('click',function(e) {
-    e.preventDefault();
-     buttonDeco.stop().toggleClass('button_deco');
-     if(buttonDeco.hasClass('button_deco')){
-       gnb.css({transform:'translateY(0)',transition:'all 500ms'});
-      }else{
-       gnb.css({transform:'translateY(-60px)',transition:'all 500ms'});
-      }
-  });
-
-  //   //버튼 눌렀을 떄--
-  // var buttonDeco = $('#buttonDeco')
-  //   menuButton.on('click',function(e){
-  //     e.preventDefault();
-  //     buttonDeco.stop().toggleClass('button_deco');
-  //     if(buttonDeco.hasClass('button_deco')){
-  //       gnb.css({position:'fixed', top: '-60px', zIndex:'6000'}).animate({top:'0'});
-  //       menuButton.animate({position:'fixed', top: '60px'});
-  //     }else{
-  //       gnb.animate({position:'fixed', top: '-60px'});
-  //       menuButton.animate({position:'fixed', top: '0'});
-  //     }
-  //       //버그가 많이 일어나고 있습니다. 도와주세요.
-  //   });
-
-
   // 배너의 maple effect _________________
   var addBanner = $('#addBanner');
   var maple_1_l = $('.maple_1_l');
@@ -98,88 +27,7 @@
     maple_2_r.css({transform:'translate('+ -percentX + 'px, '+ -percentY / 8 + 'px)'});
     maple_3_r.css({transform:'translate('+ 1.8 * percentX + 'px, '+ percentY / 8 + 'px)'});
   });
-
-  // addBanner 슬라이드 ____________________
-  var bannerUl = $('.wrap_banner').children('ul');
-  var banner_li = bannerUl.children()
-  var clone_li = banner_li.last().clone();
-
-  var indicatorBox = $('.btn_indicate');
-  var indicator_li = indicatorBox.children();
-  var clone_indi = indicator_li.last().clone();
-
-  var btn_indi = indicatorBox.children('li');
-  var btnLeft = $('.btn_left');
-  var btnRight = $('.btn_right');
-
-  var i = 1;
-
-  bannerUl.prepend(clone_li);
-  banner_li = bannerUl.children();
-  var banner_i = banner_li.length;
-
-  indicatorBox.prepend(clone_indi);
-indicatorBox = $('.btn_indicate');
-indicator_li = indicatorBox.children();
-indicator_li.first().hide(); //---------------------------
-
-bannerUl.css({width:banner_i*100+'%', marginLeft: -100+'%'});
-banner_li.css({width: 100/banner_i+'%'});
-
-
-function MoveUl(e){
-  e.preventDefault();
-  var _this = $(this);
-  i = _this.index();
-  bannerUl.stop().animate({marginLeft: -(100*i)+'%'});
-  indicator_li.eq(i).css({backgroundColor:'#F18C00'}).siblings().css({backgroundColor:'#E4DBCF'});
-  // console.log(i);
-};
-
-btn_indi.on('click',MoveUl); // indicate 클릭
-
-btnLeft.on('click',MoveUl_l);
-
-// console.log(i);
-
- indicator_li.eq(i).css({backgroundColor:'#F18C00'}).siblings().css({backgroundColor:'#E4DBCF'});
-
-function MoveUl_l(){
-  i -= 1 ;
-  if(i<=0){
-    
-    bannerUl.stop().animate({marginLeft: -(100*i)+'%'},function() {
-  //console.log(i);
-    // i = banner_i-1;
-    bannerUl.css({marginLeft: -(100*i)+'%'});
-    });
-    i = banner_i-1;
-    // console.log(i);
-    indicator_li.eq(i).css({backgroundColor:'#F18C00'}).siblings().css({backgroundColor:'#E4DBCF'});
-   }else{
-     bannerUl.stop().animate({marginLeft: -(100*i)+'%'});
-     indicator_li.eq(i).css({backgroundColor:'#F18C00'}).siblings().css({backgroundColor:'#E4DBCF'});
-  }
-};
-btnRight.on('click',MoveUl_r);
-
-
-function MoveUl_r(){
-  i += 1;
-  indicator_li.eq(i).css({backgroundColor:'#F18C00'}).siblings().css({backgroundColor:'#E4DBCF'});
-  if(i>=banner_i-1){
-    i=banner_i-1;
-    bannerUl.stop().animate({marginLeft: -(100*i)+'%'},function() {
-    i = 0;
-    bannerUl.css({marginLeft: -(100*i)+'%'});
-    });
-  }else{
-    bannerUl.stop().animate({marginLeft: -(100*i)+'%'});
-  }
-  //console.log(i);
-};
-
- window.setInterval(MoveUl_r,5500);
+ //-------------------------------------
 
  // 추천도서, 신간도서_______ btn_book_____
  //추천도서__
@@ -270,7 +118,7 @@ function MoveUl_r(){
 
     btnL.on('click',function(){
     var ulml = parseInt(noticeUl.css('marginLeft'));
-      noticeUl.animate({marginLeft: ulml + 174 + 'px'},function() {
+      noticeUl.stop(true,false).animate({marginLeft: ulml + 174 + 'px'},function() {
         ulml = parseInt(noticeUl.css('marginLeft'));
         if(ulml >= 0){
         btnL.hide();
@@ -297,6 +145,39 @@ function MoveUl_r(){
     
   });//btnR.on
   })();
+
+  // 오프라인 매장 지도 시작 .box_map_store------------------------
+  // *핀셋 버튼 누르면 우측에 정보 뜨기
+
+  (function(){
+    var map = $('.map');
+    var map_li = map.children('li');
+    var boxContents = $('.box_contents');
+    var boxcon_li = boxContents.children('li');
+ 
+    map_li.on('click',function(e){
+      e.preventDefault();
+      var _this = $(this);
+      var _this_in = _this.index();
+      _this.siblings().removeClass('actPoint');
+      _this.addClass('actPoint');
+      boxcon_li.eq(_this_in).siblings().removeClass('active');
+      boxcon_li.eq(_this_in).addClass('active');
+    });
+
+  // *매장 이미지 사진
+  // var _dataImg = boxcon_li.eq(1).find('.store_img');
+  // console.log(_dataImg);
+
+   for(var i=0; i < map_li.length; i++){
+    var _store_img = boxcon_li.eq(i).find('.store_img');
+    var _dataImg = map_li.eq(i).attr('data-img');
+    // console.log(_dataImg);
+    _store_img.css({backgroundImage:'url("../img/main/store/' + _dataImg+ '.jpg")'});
+   }
+
+  })();//즉시실행함수 끝
+  // 오프라인 매장 지도 끝  //.box_map_store-----------------------
   
 
 })(this.jQuery);
---
+++ js/src/sub_ebook.js
@@ -0,0 +1,83 @@
+(function($){
+  // notice__________________________
+
+ var noticeUl = $('.menu_notice');
+ var notice_dt = $('.menu_notice').children().children().children('dt');
+ var notice_dd = $('.menu_notice').children().children().children('dd');
+
+ notice_dt.on('click',function(e){
+  e.preventDefault();
+  var _this = $(this);
+  var _this_li = _this.parent().parent('li');
+  var _this_dd = _this_li.children().children('dd');
+
+  notice_dd.css({display:'none'});
+  _this_dd.css({display:'block'});
+
+  notice_dt.css({backgroundColor:'#7F7F7F'});
+  _this.css({backgroundColor:'#F18C00'});
+ });
+
+  //좌,우 버튼 움직이기
+  (function(){
+    var btnL = $('.btn_l');
+    var btnR = $('.btn_r');
+    btnL.hide();
+
+    btnL.on('click',function(){
+    var ulml = parseInt(noticeUl.css('marginLeft'));
+      noticeUl.stop(true,false).animate({marginLeft: ulml + 174 + 'px'},function() {
+        ulml = parseInt(noticeUl.css('marginLeft'));
+        if(ulml >= 0){
+        btnL.hide();
+        btnR.show();
+      }else{
+        btnL.show();
+        btnR.show(); 
+       }//if문,else
+      });
+  
+    });//btnL.on
+    btnR.on('click',function(){
+    var ulml = parseInt(noticeUl.css('marginLeft'));
+    noticeUl.animate({marginLeft: ulml -174+ 'px'},function(){
+      ulml = parseInt(noticeUl.css('marginLeft'));
+      if(ulml < -522){
+      btnL.show();
+      btnR.hide();
+    }else{
+      btnL.show();
+      btnR.show();
+     }//if문,else
+    });
+    
+  });//btnR.on
+  })();
+  //--------------------------------------
+  //무료도서__
+ (function(){
+  var boxRecommend = $('.box_free');
+  var reco_ul = boxRecommend.find('.list_book');
+  var reco_btn_l = boxRecommend.find('.bnt_left_');
+  var reco_btn_r = boxRecommend.find('.bnt_right_');
+
+  reco_btn_l.on('click',function(){
+    var reulmg = parseInt(reco_ul.css('marginLeft'));
+    //reco_ul.css({marginLeft: reulmg + 950 + 'px'});
+    if(reulmg == 0){
+      reco_ul.css({marginLeft:'-950px'});
+    }else{
+      reco_ul.css({marginLeft: reulmg + 950 + 'px'});
+    }
+  });// reco_btn_l.on('click',function()
+  reco_btn_r.on('click',function(){
+    var reulmg = parseInt(reco_ul.css('marginLeft'));
+    //reco_ul.css({marginLeft: reulmg - 950 + 'px'});
+    if(reulmg == -950){
+      reco_ul.css({marginLeft: 0});
+    }else{
+      reco_ul.css({marginLeft: reulmg + -950 + 'px'});
+    }
+  });// reco_btn_r.on('click',function()
+ })();
+})(this.jQuery);
